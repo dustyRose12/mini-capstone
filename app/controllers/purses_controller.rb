49c4303f7e70d5bb6b76822271f66b30508225a3
dustyRose12 <dustyrose125@gmail.com>
2017-08-31 21:26:16 -0500

    mini-capstone-app

--- app/controllers/purses_controller.rb
+++ app/controllers/purses_controller.rb
@@ -1,13 +1,22 @@
 class PursesController < ApplicationController
 
-  def one_purse_action
-    @purse = Purse.first
-    render 'one_purse_page.html.erb'
+  # def one_purse_action
+  #   @purse = Purse.first
+  #   render 'one_purse_page.html.erb'
+  # end
+
+  # def all_purses_action
+  #   @purses = Purse.all
+  #   render 'all_purses_page.html.erb'
+  # end
+
+  def index
+    @purses = Purse.all
   end
 
-  def all_purses_action
-    @purses = Purse.all
-    render 'all_purses_page.html.erb'
+  def show
+    @purse = Purse.find(params[:id])
   end
+  
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get '/one_purse_url' => 'purses#one_purse_action'
-  get '/all_purses_url' => 'purses#all_purses_action'
+  # get '/one_purse_url' => 'purses#one_purse_action'
+  # get '/all_purses_url' => 'purses#all_purses_action'
+
+  get '/' => 'purses#index'
+  get 'purses' => 'purses#index'
+  get 'purses/:id' => 'purses#show'
+
+
 end
