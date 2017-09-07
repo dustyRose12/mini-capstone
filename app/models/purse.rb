b2242d2c6d58d319e5101dce7286e25bdcef8487
dustyRose12 <dustyrose125@gmail.com>
2017-09-06 21:21:42 -0500

    mini-capstone-app

--- app/models/purse.rb
+++ app/models/purse.rb
@@ -1,2 +1,19 @@
 class Purse < ApplicationRecord
+
+  def sale_message
+    if price < 2
+      return "Discount Item!"
+    else
+      return "Everyday Value!"
+    end
+  end
+
+  def tax
+    return price * 0.09
+  end
+
+  def total
+    return price + tax
+  end
+
 end
